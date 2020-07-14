f6eb7a2ac5f0c80a4a0fcf620c11b170ab1411f2
Gabriel Pivaro <gabrconc@gmail.com>
2020-07-14 04:14:36 +0000

    Moved allowed hosts to /etc/config.json

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [config["ALLOWED_HOSTS_1"],config["ALLOWED_HOSTS_2"],config["ALLOWED_HOSTS_3"]]
 

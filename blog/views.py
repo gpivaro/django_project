e9e2670373b09b446c35a5a008900b84d6d8b78d
Gabriel Pivaro <gabrconc@gmail.com>
2021-02-26 20:25:30 -0600

    blog view

--- blog/views.py
+++ blog/views.py
@@ -51,7 +51,7 @@ class PostDetailView(DetailView):
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ["title", "content"]
+    fields = ["title", "content", "url_info"]
 
     def form_valid(self, form):
         form.instance.author = self.request.user

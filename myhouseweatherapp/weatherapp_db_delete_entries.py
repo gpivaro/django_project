7c9fdb73ccf37eed737acdf3bd28fa06b2b1d769
Gabriel Pivaro <gabrconc@gmail.com>
2021-08-22 16:56:08 -0500

    weather app auto delete records from sql database

---
+++ myhouseweatherapp/weatherapp_db_delete_entries.py
@@ -0,0 +1,83 @@
+import mysql.connector
+import os
+import json
+import datetime as dt
+
+# Create a string with the time delta of x days
+target_date = dt.datetime.date(dt.datetime.now() - dt.timedelta(days=116))
+print(f"""The target date is {target_date.strftime("%Y-%m-%d")}.\n""")
+
+
+with open("/etc/config.json") as config_file:
+    config = json.load(config_file)
+
+
+# Create a function to connect to the MYSQL server
+def database_connect(hostname, port, username, password, database=""):
+    mydb = mysql.connector.connect(
+        host=hostname, port=port, user=username, passwd=password, database=database
+    )
+    return mydb
+
+
+# Return an object containing the MYSQL connection
+mydb = database_connect(
+    config.get("MYSQL_HOSTNAME"),
+    config.get("MYSQL_PORT"),
+    config.get("MYSQL_USERNAME"),
+    config.get("MYSQL_PASSWORD"),
+    database="djangowebsite",
+)
+print(f"""Message from database: {mydb}\n""")
+
+
+# Create the cursor to manipute databases
+my_cursor = mydb.cursor()
+
+
+# Select values that match the criteria
+my_cursor.execute(
+    f"""SELECT count(*) 
+        FROM djangowebsite.myhouseweatherapp_weather 
+        where meas_time < '{target_date.strftime("%Y-%m-%d")}'
+        """
+)
+
+for x in my_cursor:
+    print(
+        f"""There are {x[0]} records that are older than {target_date.strftime("%Y-%m-%d")} at ({dt.datetime.now()}).\n"""
+    )
+
+with open("~/db_records_log.txt", "a") as f:
+    f.write(
+        f"""There are {x[0]} records that are older than {target_date.strftime("%Y-%m-%d")} (at {dt.datetime.now()}).\n"""
+    )
+
+# Delete values that match the criteria
+my_cursor.execute(
+    f"""DELETE 
+        FROM djangowebsite.myhouseweatherapp_weather 
+        WHERE meas_time < '{target_date.strftime("%Y-%m-%d")}';
+        """
+)
+
+
+# commit changes
+mydb.commit()
+
+
+# Select values that match the criteria
+my_cursor.execute(
+    f"""SELECT count(*) 
+        FROM djangowebsite.myhouseweatherapp_weather 
+        where meas_time < '{target_date.strftime("%Y-%m-%d")}' 
+        order by meas_time desc 
+        limit 1;
+        """
+)
+
+for x in my_cursor:
+    print(
+        f"""There are {x[0]} records that are older than {target_date.strftime("%Y-%m-%d")}."""
+    )
+

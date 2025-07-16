076042faf721297f7e64ade6c1287e614b98a852
gpivaro <Gabriel Pivaro>
2025-07-15 22:05:45 -0500

    Fix Django warning "Auto-created primary key"

--- django_project/settings.py
+++ django_project/settings.py
@@ -146,6 +146,10 @@ DATABASES = {
     }
 }
 
+# configure a default auto field for your project
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
